f92a1d971e4d5559f964be38b3bb26761f5f3ff0
Sayar Banerjee <30318719+Sayar1106@users.noreply.github.com>
2020-12-02 19:02:50 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -14,8 +14,9 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+ds = ### YOUR CODE HERE ###
 
+x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
@@ -48,8 +49,6 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-
-    return x_df, y_df
     
 
 def main():
@@ -63,15 +62,11 @@ def main():
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
-    x, y = clean_data(ds)
-    x_train, x_test, y_train, y_test = train_test_split(x, y)
+
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-
-    os.makedirs('outputs', exist_ok=True)
-    joblib.dump(model, 'outputs/project_model.joblib')
 
 if __name__ == '__main__':
     main()
